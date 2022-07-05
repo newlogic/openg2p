8a5635f4be3c40c353f508ee2f74ee315792e8c3
Michael John Gonzales <michaelgonzales2117@gmail.com>
2022-07-06 05:06:41 +0800

    added assert

--- g2p_programs/tests/test_programs.py
+++ g2p_programs/tests/test_programs.py
@@ -7,6 +7,8 @@ class ProgramTest(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(ProgramTest, cls).setUpClass()
+
+        # Initial Setup of Variables
         cls.registrant_1 = cls.env["res.partner"].create(
             {
                 "family_name": "Doe",
@@ -35,19 +37,44 @@ class ProgramTest(TransactionCase):
         self.program_1.write(
             {"program_membership_ids": [(0, 0, {"partner_id": self.registrant1.id})]}
         )
+        self.assertEqual(
+            self.program_1.program_membership_ids[0].partner_id.id, self.registrant1.id
+        )
+
+        # Check if beneficiaries_count compute is computing as expected
+        self.assertEqual(self.program_1.beneficiaries_count, 1)
 
     def test_add_group(self):
         self.program_2.write(
             {"program_membership_ids": [(0, 0, {"partner_id": self.group_1.id})]}
         )
+        self.assertEqual(
+            self.program_2.program_membership_ids[0].partner_id.id, self.group_1.id
+        )
+
+        # Check if beneficiaries_count compute is computing as expected
+        self.assertEqual(self.program_2.beneficiaries_count, 1)
 
     def test_enroll(self):
         self.program_1.enroll_eligible_registrants()
+        self.assertEqual(self.program_1.program_membership_ids[0].state, "enrolled")
         self.program_2.enroll_eligible_registrants()
+        self.assertEqual(self.program_2.program_membership_ids[0].state, "enrolled")
+
+        # Check if eligible_beneficiaries_count compute is computing as expected
+        self.assertEqual(self.program_1.eligible_beneficiaries_count, 1)
+        self.assertEqual(self.program_2.eligible_beneficiaries_count, 1)
 
     def test_add_cycle(self):
         self.program_1.create_new_cycle()
+        self.assertEqual(len(self.program_1.cycle_ids), 1)
         self.program_2.create_new_cycle()
+        self.assertEqual(len(self.program_2.cycle_ids), 1)
+
+        # Check if cycles_count compute is computing as expected
+        self.assertEqual(self.program_1.cycles_count, 1)
+        self.assertEqual(self.program_2.cycles_count, 1)
+
         self.cycle1 = self.env["g2p.cycle"].search(
             [("id", "=", self.program_1.cycle_ids[0].id)]
         )
@@ -57,19 +84,39 @@ class ProgramTest(TransactionCase):
 
     def test_cycle_copy_beneficiary(self):
         self.cycle1.copy_beneficiaries_from_program()
+        self.assertEqual(
+            self.cycle1.cycle_membership_ids[0].partner_id.id, self.registrant1.id
+        )
         self.cycle2.copy_beneficiaries_from_program()
+        self.assertEqual(
+            self.cycle2.cycle_membership_ids[0].partner_id.id, self.group_1.id
+        )
+
+        # Check if members_count compute is computing as expected
+        self.assertEqual(self.cycle1.members_count, 1)
+        self.assertEqual(self.cycle2.members_count, 1)
 
     def test_cycle_prepare_entitlement(self):
         self.cycle1.prepare_entitlement()
+        self.assertEqual(len(self.cycle1.entitlement_ids), 1)
         self.cycle2.prepare_entitlement()
+        self.assertEqual(len(self.cycle2.entitlement_ids), 1)
+
+        # Check if entitlements_count compute is computing as expected
+        self.assertEqual(self.cycle1.entitlements_count, 1)
+        self.assertEqual(self.cycle2.entitlements_count, 1)
 
     def test_cycle_to_approve(self):
         self.cycle1.to_approve()
+        self.assertEqual(self.cycle1.state, "to_approve")
         self.cycle2.to_approve()
+        self.assertEqual(self.cycle2.state, "to_approve")
 
     def test_cycle_approve(self):
         self.cycle1.approve()
+        self.assertEqual(self.cycle1.state, "approved")
         self.cycle2.approve()
+        self.assertEqual(self.cycle2.state, "approved")
 
     def test_deduplication(self):
         self.program_1.deduplicate_beneficiaries()
@@ -77,4 +124,6 @@ class ProgramTest(TransactionCase):
 
     def test_end(self):
         self.program_1.end_program()
+        self.assertEqual(self.program_1.state, "ended")
         self.program_2.end_program()
+        self.assertEqual(self.program_2.state, "ended")
--- g2p_registrant/tests/test_registrant.py
+++ g2p_registrant/tests/test_registrant.py
@@ -7,6 +7,8 @@ class RegistrantTest(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(RegistrantTest, cls).setUpClass()
+
+        # Initial Setup of Variables
         cls.registrant_1 = cls.env["res.partner"].create(
             {
                 "family_name": "Doe",
@@ -34,22 +36,36 @@ class RegistrantTest(TransactionCase):
         self.group_1.write(
             {"group_membership_ids": [(0, 0, {"individual": self.registrant1.id})]}
         )
+        self.assertEqual(
+            self.group_1.group_membership_ids[0].individual.id, self.registrant1.id
+        )
 
     def test_assign_member(self):
         self.registrant_2.write(
             {"individual_membership_ids": [(0, 0, {"group": self.group_2.id})]}
+        )
+        self.assertEqual(
+            self.registrant_2.individual_membership_ids[0].group.id, self.group_2.id
         )
 
     def test_add_relationship_1(self):
         self.registrant_1.write(
             {"related_1_ids": [(0, 0, {"registrant2": self.registrant_2.id})]}
         )
+        self.assertEqual(
+            self.registrant_1.related_1_ids[0].registrant2.id, self.registrant_2.id
+        )
         self.group_1.write(
             {"related_1_ids": [(0, 0, {"registrant2": self.group_2.id})]}
         )
+        self.assertEqual(self.group_1.related_1_ids[0].registrant2.id, self.group_2.id)
 
     def test_add_relationship_2(self):
         self.registrant_2.write(
             {"related_2_ids": [(0, 0, {"registrant1": self.registrant_1.id})]}
         )
+        self.assertEqual(
+            self.registrant_2.related_2_ids[0].registrant1.id, self.registrant_1.id
+        )
         self.group2.write({"related_2_ids": [(0, 0, {"registrant1": self.group_1.id})]})
+        self.assertEqual(self.group2.related_2_ids[0].registrant1.id, self.group_1.id)

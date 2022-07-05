1ae46036b298fc2b4233cefd89102a58d48c9099
Edwin N Gonzales <edwin@pithtech.net>
2022-07-05 09:46:27 +0800

    Delete g2p_additional_data directory

--- g2p_additional_data/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Part of Newlogic G2P. See LICENSE file for full copyright and licensing details.
-
-from . import datasource
-from . import additional_data
-from . import additional_data_tags
-from . import registrant_additional_data
-from . import registrant

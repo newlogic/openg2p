c70e2874dfac7041822c3290e1704ce27f0c5684
Edwin Gonzales <edwin@pithtech.net>
2022-07-06 12:08:34 +0800

    Added the function: query_members_aggregate

--- g2p_registrant/models/group.py
+++ g2p_registrant/models/group.py
@@ -2,6 +2,7 @@
 import logging
 
 from odoo import fields, models
+from odoo.osv import expression
 
 _logger = logging.getLogger(__name__)
 
@@ -42,6 +43,85 @@ class G2PGroup(models.Model):
         if criteria is not None:
             domain.extend(criteria)
         return self.env["res.partner"].search_count(domain)
+
+    def query_members_aggregate(
+        self, membership_kind_domain=None, individual_domain=None
+    ):
+        domain = [("is_registrant", "=", True), ("is_group", "=", True)]
+        query_obj = self.env["res.partner"]._where_calc(domain)
+
+        membership_alias = query_obj.left_join(
+            "res_partner", "id", "g2p_group_membership", "group", "id"
+        )
+        individual_alias = query_obj.left_join(
+            membership_alias, "individual", "res_partner", "id", "individual"
+        )
+        membership_kind_rel_alias = query_obj.left_join(
+            membership_alias,
+            "id",
+            "g2p_group_membership_g2p_group_membership_kind_rel",
+            "g2p_group_membership_id",
+            "id",
+        )
+        query_obj.left_join(
+            membership_kind_rel_alias,
+            "g2p_group_membership_kind_id",
+            "g2p_group_membership_kind",
+            "id",
+            "id",
+        )
+
+        if membership_kind_domain:
+            membership_query_obj = expression.expression(
+                model=self.env["g2p.group.membership"],
+                domain=membership_kind_domain,
+                alias=membership_alias,
+            ).query
+            (
+                membership_from_clause,
+                membership_where_clause,
+                membership_where_params,
+            ) = membership_query_obj.get_sql()
+            # _logger.info("SQL DEBUG: Membership Kind Query: From:%s, Where:%s, Params:%s" %
+            #   (membership_from_clause,membership_where_clause,membership_where_params))
+            query_obj.add_where(membership_where_clause, membership_where_params)
+
+        if individual_domain:
+            individual_query_obj = expression.expression(
+                model=self.env["res.partner"],
+                domain=individual_domain,
+                alias=individual_alias,
+            ).query
+            (
+                individual_from_clause,
+                individual_where_clause,
+                individual_where_params,
+            ) = individual_query_obj.get_sql()
+            # _logger.info("SQL DEBUG: Individual Query: From:%s, Where:%s, Params:%s" %
+            #   (individual_from_clause,individual_where_clause,individual_where_params))
+            query_obj.add_where(individual_where_clause, individual_where_params)
+
+        select_query, select_params = query_obj.select()
+        _logger.info(
+            "SQL DEBUG: SQL query: %s, params: %s" % (select_query, select_params)
+        )
+
+        self._cr.execute(select_query, select_params)
+        results = self._cr.dictfetchall()
+        _logger.info("SQL DEBUG: SQL Query Result: %s" % results)
+
+    def test_query(self):
+        self.query_members_aggregate(
+            membership_kind_domain=[("end_date", "=", None)],
+            individual_domain=[
+                ("gender", "=", "Female"),
+                ("birthdate", "<", fields.Datetime.now()),
+            ],
+        )
+        self.query_members_aggregate(
+            membership_kind_domain=[("end_date", "=", None)],
+            individual_domain=[("gender", "=", "Male")],
+        )
 
 
 class G2PGroupKind(models.Model):

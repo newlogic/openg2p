1b144ffbc5926dd4c2c76b98239a9aa8967cfbda
Michael John Gonzales <michaelgonzales2117@gmail.com>
2022-07-13 07:02:01 +0800

    dedup by phone and id initial commit

--- g2p_programs/models/managers/deduplication_manager.py
+++ g2p_programs/models/managers/deduplication_manager.py
@@ -20,6 +20,7 @@ class DeduplicationManager(models.Model):
         new_managers = [
             ("g2p.deduplication.manager.default", "Default Deduplication"),
             ("g2p.deduplication.manager.phone_number", "Phone Number Deduplication"),
+            ("g2p.deduplication.manager.id_dedup", "ID Deduplication"),
         ]
         for new_manager in new_managers:
             if new_manager not in selection:
@@ -169,7 +170,135 @@ class IDDocumentDeduplication(models.Model):
     )
 
     def deduplicate_beneficiaries(self, states):
-        pass
+        for rec in self:
+            duplicate_beneficiaries = []
+            program = rec.program_id
+            beneficiaries = program.get_beneficiaries(states)
+            # duplicates
+            _logger.info("Deduplicate beneficiaries: %s", beneficiaries)
+            if program.target_type == "group":
+                duplicate_beneficiaries = (
+                    self._check_duplicate_by_group_with_individual(beneficiaries)
+                )
+            return len(duplicate_beneficiaries)
+
+    def _record_duplicate(self, manager, beneficiary_ids, reason):
+        """
+        This method is used to record a duplicate beneficiary.
+        :param beneficiary: The beneficiary.
+        :param reason: The reason.
+        :param comment: The comment.
+        :return:
+        """
+
+        # TODO: check this group does not exist already with the same manager and the same beneficiaries or
+        #  a subset of them.
+        #  1. If the state has been changed to no_duplicate, then we should not record it as duplicate unless there are
+        #  additional beneficiaries in the group.
+        #  2. Otherwise we update the record.
+
+        _logger.info("Record duplicate: %s", beneficiary_ids)
+        data = {
+            "beneficiary_ids": [(6, 0, beneficiary_ids)],
+            "state": "duplicate",
+            "reason": reason,
+            "deduplication_manager_id": manager,
+        }
+        _logger.info("Record duplicate: %s", data)
+
+        self.env["g2p.program.membership.duplicate"].create(data)
+
+    def _check_duplicate_by_group_with_individual(self, beneficiaries):
+        """
+        This method is used to check if there are any duplicates among the individuals docs.
+        :param beneficiary_ids: The beneficiaries.
+        :return:
+        """
+        _logger.info("-" * 100)
+        group_ids = beneficiaries.mapped("partner_id.id")
+        group_memberships = self.env["g2p.group.membership"].search(
+            [("group", "in", group_ids)]
+        )
+        group = self.env["res.partner"].search([("id", "in", group_ids)])
+        _logger.info("group_memberships: %s", group_memberships)
+
+        individuals_ids = [rec.individual.id for rec in group_memberships]
+        _logger.info("individuals_ids: %s", individuals_ids)
+        individual_id_docs = []
+        # Check ID Docs of each individual
+        for i in group_memberships:
+            individual_id_docs += [
+                x.id
+                for x in i.individual.reg_ids
+                if x.id_type in self.supported_id_document_types
+            ]
+
+        # Check ID Docs of each group
+        for ix in group:
+            individual_id_docs += [
+                x.id
+                for x in ix.reg_ids
+                if x.id_type in self.supported_id_document_types
+            ]
+
+        _logger.info("individual_id_docs: %s", individual_id_docs)
+        duplicate_individuals_id_docs = [
+            item
+            for item, count in collections.Counter(individual_id_docs).items()
+            if count > 1
+        ]
+        _logger.info(
+            "Duplicate Individuals ID Docs IDs: %s", duplicate_individuals_id_docs
+        )
+
+        duplicate_individuals_ids = self.env["res.partner"].search(
+            [("reg_ids", "in", duplicate_individuals_id_docs)]
+        )
+        duplicate_individuals = [x.id for x in duplicate_individuals_ids]
+
+        group_with_duplicates = self.env["g2p.group.membership"].search(
+            [("group", "in", group_ids), ("individual", "in", duplicate_individuals)]
+        )
+
+        _logger.info("group_with_duplicates: %s", group_with_duplicates)
+        group_of_duplicates = {}
+        for group_membership in group_with_duplicates:
+            _logger.info(
+                "group_membership.individual.id: %s -> %s"
+                % (group_membership.individual.id, group_membership.group.id)
+            )
+            if group_membership.individual.id not in group_of_duplicates:
+                group_of_duplicates[group_membership.individual.id] = []
+            group_of_duplicates[group_membership.individual.id].append(
+                group_membership.group.id
+            )
+
+        _logger.info("group_of_duplicates: %s", group_of_duplicates)
+        for _individual, group_ids in group_of_duplicates.items():
+
+            duplicate_beneficiaries = beneficiaries.filtered(
+                lambda rec: rec.partner_id.id in group_ids
+            )
+            duplicate_beneficiariy_ids = duplicate_beneficiaries.mapped("id")
+
+            self._record_duplicate(self, duplicate_beneficiariy_ids, "Duplicate IDs")
+
+            duplicated_enrolled = duplicate_beneficiaries.filtered(
+                lambda rec: rec.state == "enrolled"
+            )
+            if len(duplicated_enrolled) == 1:
+                # If there is only 1 enrolled that is duplicated, the enrolled one should not be marked as duplicate.
+                # otherwise if there is more than 1, then there is a problem!
+                # TODO: check how to handle this
+                duplicated_enrolled.write({"state": "enrolled"})
+                duplicate_beneficiaries = duplicate_beneficiaries.filtered(
+                    lambda rec: rec.state != "enrolled"
+                )
+            duplicate_beneficiaries.filtered(
+                lambda rec: rec.state not in ["exited", "not_eligible", "duplicated"]
+            ).write({"state": "duplicated"})
+
+        return group_with_duplicates
 
 
 class PhoneNumberDeduplication(models.Model):
@@ -185,9 +314,131 @@ class PhoneNumberDeduplication(models.Model):
     # # if set, we verify that the phone number match a given regex
     # phone_regex = fields.Char(string="Phone Regex")
 
-    def check_duplicates(self, program_memberships):
-        # TODO: check if beneficiaries still match the criterias
-        return True
+    def deduplicate_beneficiaries(self, states):
+        for rec in self:
+            duplicate_beneficiaries = []
+            program = rec.program_id
+            beneficiaries = program.get_beneficiaries(states)
+            # duplicates
+            _logger.info("Deduplicate beneficiaries: %s", beneficiaries)
+            if program.target_type == "group":
+                duplicate_beneficiaries = (
+                    self._check_duplicate_by_group_with_individual(beneficiaries)
+                )
+            return len(duplicate_beneficiaries)
+
+    def _record_duplicate(self, manager, beneficiary_ids, reason):
+        """
+        This method is used to record a duplicate beneficiary.
+        :param beneficiary: The beneficiary.
+        :param reason: The reason.
+        :param comment: The comment.
+        :return:
+        """
+
+        # TODO: check this group does not exist already with the same manager and the same beneficiaries or
+        #  a subset of them.
+        #  1. If the state has been changed to no_duplicate, then we should not record it as duplicate unless there are
+        #  additional beneficiaries in the group.
+        #  2. Otherwise we update the record.
+
+        _logger.info("Record duplicate: %s", beneficiary_ids)
+        data = {
+            "beneficiary_ids": [(6, 0, beneficiary_ids)],
+            "state": "duplicate",
+            "reason": reason,
+            "deduplication_manager_id": manager,
+        }
+        _logger.info("Record duplicate: %s", data)
+
+        self.env["g2p.program.membership.duplicate"].create(data)
+
+    def _check_duplicate_by_group_with_individual(self, beneficiaries):
+        """
+        This method is used to check if there are any duplicates among the individuals docs.
+        :param beneficiary_ids: The beneficiaries.
+        :return:
+        """
+        _logger.info("-" * 100)
+        group_ids = beneficiaries.mapped("partner_id.id")
+        group_memberships = self.env["g2p.group.membership"].search(
+            [("group", "in", group_ids)]
+        )
+        group = self.env["res.partner"].search([("id", "in", group_ids)])
+        _logger.info("group_memberships: %s", group_memberships)
+
+        individuals_ids = [rec.individual.id for rec in group_memberships]
+        _logger.info("individuals_ids: %s", individuals_ids)
+        individual_phone_numbers = []
+        # Check ID Docs of each individual
+        for i in group_memberships:
+            individual_phone_numbers += [x.id for x in i.individual.phone_number_ids]
+
+        # Check ID Docs of each group
+        for ix in group:
+            individual_phone_numbers += [x.id for x in ix.phone_number_ids]
+
+        _logger.info("individual_phone_numbers: %s", individual_phone_numbers)
+        duplicate_individuals_phone_numbers = [
+            item
+            for item, count in collections.Counter(individual_phone_numbers).items()
+            if count > 1
+        ]
+        _logger.info(
+            "Duplicate Individuals Phone Number IDs: %s",
+            duplicate_individuals_phone_numbers,
+        )
+
+        duplicate_individuals_ids = self.env["res.partner"].search(
+            [("phone_number_ids", "in", duplicate_individuals_phone_numbers)]
+        )
+        duplicate_individuals = [x.id for x in duplicate_individuals_ids]
+
+        group_with_duplicates = self.env["g2p.group.membership"].search(
+            [("group", "in", group_ids), ("individual", "in", duplicate_individuals)]
+        )
+
+        _logger.info("group_with_duplicates: %s", group_with_duplicates)
+        group_of_duplicates = {}
+        for group_membership in group_with_duplicates:
+            _logger.info(
+                "group_membership.individual.id: %s -> %s"
+                % (group_membership.individual.id, group_membership.group.id)
+            )
+            if group_membership.individual.id not in group_of_duplicates:
+                group_of_duplicates[group_membership.individual.id] = []
+            group_of_duplicates[group_membership.individual.id].append(
+                group_membership.group.id
+            )
+
+        _logger.info("group_of_duplicates: %s", group_of_duplicates)
+        for _individual, group_ids in group_of_duplicates.items():
+
+            duplicate_beneficiaries = beneficiaries.filtered(
+                lambda rec: rec.partner_id.id in group_ids
+            )
+            duplicate_beneficiariy_ids = duplicate_beneficiaries.mapped("id")
+
+            self._record_duplicate(
+                self, duplicate_beneficiariy_ids, "Duplicate Phone Numbers"
+            )
+
+            duplicated_enrolled = duplicate_beneficiaries.filtered(
+                lambda rec: rec.state == "enrolled"
+            )
+            if len(duplicated_enrolled) == 1:
+                # If there is only 1 enrolled that is duplicated, the enrolled one should not be marked as duplicate.
+                # otherwise if there is more than 1, then there is a problem!
+                # TODO: check how to handle this
+                duplicated_enrolled.write({"state": "enrolled"})
+                duplicate_beneficiaries = duplicate_beneficiaries.filtered(
+                    lambda rec: rec.state != "enrolled"
+                )
+            duplicate_beneficiaries.filtered(
+                lambda rec: rec.state not in ["exited", "not_eligible", "duplicated"]
+            ).write({"state": "duplicated"})
+
+        return group_with_duplicates
 
 
 class IDPhoneEligibilityManager(models.Model):
